24d07f66d4132fc1d1f3fb9ac95d3a16887a31a7
hbercam <hauris@gmail.com>
2024-08-29 00:45:41 +0000

    initfy

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -18,6 +18,7 @@ export default async function migrations(request, response) {
         const pendingMigrations = await migrationRunner(
             defaultMigrationOptions,
         );
+
         await dbClient.end();
         response.status(200).json(pendingMigrations);
     } else if (request.method === "POST") {
