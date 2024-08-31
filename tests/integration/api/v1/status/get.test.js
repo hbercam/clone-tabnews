24c54a77f1458cd4699bdfadd5b794913fdada58
Hauris H. Bergamini Campos <hauris@gmail.com>
2024-08-31 02:03:08 +0000

    fix `eslint` linting

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -18,9 +18,3 @@ test("GET to /api/v1/status should return 200", async () => {
     expect(responseBody.dependencies.database.max_connections).toEqual(100);
     expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
-
-// test.only("SQL Injection", async () => {
-//     const response = await fetch(
-//         "http://localhost:3000/api/v1/status?databaseName=';SELECT pg_sleep(5);--",
-//     );
-// });
