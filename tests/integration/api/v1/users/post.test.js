abc5f7732b694e10b6024b06b103136ed6e25b80
Hauris H. Bergamini Campos <hauris@gmail.com>
2025-06-12 14:42:23 +0000

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -96,7 +96,7 @@ describe("POST /api/v1/users", () => {
             expect(response2Body).toEqual({
                 name: "ValidationError",
                 message: "O email informado já está sendo utilizado.",
-                action: "Utilize outro email para realizar o cadastro.",
+                action: "Utilize outro email para realizar esta operação.",
                 status_code: 400,
             });
         });
@@ -141,7 +141,7 @@ describe("POST /api/v1/users", () => {
             expect(response2Body).toEqual({
                 name: "ValidationError",
                 message: "O usuário informado já está sendo utilizado.",
-                action: "Utilize outro usuário para realizar o cadastro.",
+                action: "Utilize outro usuário para realizar esta operação.",
                 status_code: 400,
             });
         });
